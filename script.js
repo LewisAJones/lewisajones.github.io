c2efc1e1aa44f11a0748e6a33d875fee28e9ee1a
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2024-09-06 13:19:25 +0100

    adjustments

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    const fish = document.querySelector('.fish');
-    const fishWidth = fish.offsetWidth;
-    const fishHeight = fish.offsetHeight;
-    const x = e.clientX - fishWidth / 2;
-    const y = e.clientY - fishHeight / 2;
-
-    fish.style.transform = `translate(${x}px, ${y}px)`;
-});
